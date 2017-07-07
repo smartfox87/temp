396b31f2e016ffaa609aa491d5f418e0fe3ba4f6
Andrei Dyminski <smoke59@mail.ru>
2017-07-07 20:15:11 +0300

    pre-final

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 $(function () {
-  $(".order").scroll(function () {
-    console.log('w');
-    if ($(".order").scrollLeft() > 0) {
-      $(".order__column--left").css({position: "fixed", marginLeft: "-150px"});
-      $(".order__wrapper").css("paddingLeft", "150px");
+  var buttonsPositionLeft = $(".ns-form__buttons").offset().left;
+  var buttonsPositionTop = $(document).outerHeight() - $(".ns-form__buttons").outerHeight() - 220;
+  if ($(document).scrollTop() > 0) {
+    $(".ns-form__buttons").offset({
+      top: buttonsPositionTop + $(document).scrollTop(),
+      left: buttonsPositionLeft + $(document).scrollLeft()
+    });
+  }
+  $(document).scroll(function () {
+    if ($(document).scrollLeft() > 0 || $(document).scrollTop() > 0) {
+      $(".ns-form__column--left").offset({top: 0, left: $(document).scrollLeft()});
+      $(".ns-form__buttons").offset({
+        top: buttonsPositionTop + $(document).scrollTop(),
+        left: buttonsPositionLeft + $(document).scrollLeft()
+      });
+      $(".ns-form__cell--caption").each(function (index, element) {
+        var left = $(this).outerWidth() * (index + 1);
+        $(this).offset({top: $(document).scrollTop(), left: left});
+      });
     }
   });
 });

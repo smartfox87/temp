0d22a6de8b1e71f963511d89efc68bfd6b3a1439
Andrei Dyminski <smoke59@mail.ru>
2017-07-08 14:07:36 +0300

    fix 1

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
-$(function () {
-  var buttonsPositionLeft = $(".ns-form__buttons").offset().left;
-  var buttonsPositionTop = $(document).outerHeight() - $(".ns-form__buttons").outerHeight() - 220;
-  if ($(document).scrollTop() > 0) {
-    $(".ns-form__buttons").offset({
-      top: buttonsPositionTop + $(document).scrollTop(),
-      left: buttonsPositionLeft + $(document).scrollLeft()
-    });
+$(window).on("load", function () {
+  var makeScrollVertical = ($(window).outerHeight() - $(document).outerHeight() < 0) ? true : false;
+
+  if (makeScrollVertical) {
+    $(".ns-form__buttons").css({bottom: "20px"})
   }
+
+  $(window).resize(function () {
+    makeScrollVertical = ($(window).outerHeight() - $(document).outerHeight() < 0) ? true : false;
+    if (makeScrollVertical) {
+      $(".ns-form__buttons").css({bottom: "20px"});
+    } else {
+      $(".ns-form__buttons").css({bottom: "auto"});
+    }
+  });
+
   $(document).scroll(function () {
-    if ($(document).scrollLeft() > 0 || $(document).scrollTop() > 0) {
-      $(".ns-form__column--left").offset({top: 0, left: $(document).scrollLeft()});
-      $(".ns-form__buttons").offset({
-        top: buttonsPositionTop + $(document).scrollTop(),
-        left: buttonsPositionLeft + $(document).scrollLeft()
-      });
+    var scrollingTop = ($(document).scrollTop() > 0) ? true : false;
+    var scrollingLeft = ($(document).scrollLeft() > 0) ? true : false;
+
+    if (scrollingTop) {
+      $(".ns-form__buttons").css({bottom: 20 - $(document).scrollTop()});
       $(".ns-form__cell--caption").each(function (index, element) {
         var left = $(this).outerWidth() * (index + 1);
         $(this).offset({top: $(document).scrollTop(), left: left});
       });
     }
+
+    if (scrollingLeft) {
+      $(".ns-form__column--left").offset({top: 0, left: $(document).scrollLeft()});
+      $(".ns-form__buttons").css({left: 20 + $(document).scrollLeft()});
+    }
   });
 });
+

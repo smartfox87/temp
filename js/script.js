219cd637b0b74971b9ec60e08c85d4f3a78bfcc8
Andrei Dyminski <smoke59@mail.ru>
2017-07-09 11:20:05 +0300

    fix 3

--- js/script.js
+++ js/script.js
@@ -22,14 +22,14 @@ $(window).on("load", function () {
       $(".ns-form__buttons").css({bottom: 20 - $(document).scrollTop()});
       $(".ns-form__cell--caption").each(function (index, element) {
         var left = $(this).outerWidth() * (index + 1);
-        // $(this).css({top: $(document).scrollTop(), left: left});
-        $(this).offset({top: $(document).scrollTop(), left: left});
+        $(this).css({top: $(document).scrollTop(), left: left});
+        // $(this).offset({top: $(document).scrollTop(), left: left});
       });
     }
 
     if (scrollingLeft) {
-      // $(".ns-form__column--left").css({left: $(document).scrollLeft()});
-      $(".ns-form__column--left").offset({top: 0, left: $(document).scrollLeft()});
+      $(".ns-form__column--left").css({left: $(document).scrollLeft()});
+      // $(".ns-form__column--left").offset({top: 0, left: $(document).scrollLeft()});
       $(".ns-form__buttons").css({left: 20 + $(document).scrollLeft()});
     }
   });
